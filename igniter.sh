2e1bad0bf39c4699a1d7ecd0a6fc7062cb101367
semprix <semprixd@gmail.com>
2016-05-06 10:19:46 +0800

    updates

--- igniter.sh
+++ igniter.sh
@@ -30,21 +30,41 @@ echo -e "\e[38;5;82m===>\e[0m Installing Git "
 sleep 2
 apt-get -y install git-core
 sleep 2
-# Install wget
-echo -e "\e[38;5;82m===>\e[0m Installing wget"
-sleep 2
-apt-get -y install wget
-# Install nginx
+# Install Nginx
 echo -e "\e[38;5;82m===>\e[0m Installing Nginx"
 sleep 2
 apt-get -y install nginx
+# Install nginx
+echo -e "\e[38;5;82m===>\e[0m Installing wget"
 sleep 2
+apt-get -y install wget
+sleep 2
+# Install sudo
+echo -e "\e[38;5;82m===>\e[0m Installing sudo"
+sleep 2
+apt-get -y install sudo
+sleep 2
+
+# Phase 2 Installation
+
+# Bootstrapping pip
+echo -e "\e[38;5;82m===>\e[0m Bootstrapping pip "
+wget https://bootstrap.pypa.io/get-pip.py
+# Switch to /var/www directory
+cd /var/www/
 # Install Git
-echo -e "\e[38;5;82m===>\e[0m Cloning CTFd "
-git clone git@github.com:semprix/CTFd.git
+echo -e "\e[38;5;82m===>\e[0m Cloning CTFd"
+git clone https://github.com/semprix/CTFd.git
 sleep 2
-echo -e "\e[38;5;82m===>\e[0m Preparing CTFd Engine "
-sh /root/prepare.sh
+echo -e "\e[38;5;82m===>\e[0m Exporting locale"
+export LC_ALL=C
+echo -e "\e[38;5;82m===>\e[0m Preparing CTFd Engine"
+cd /var/www/CTFd
+sleep 2
+sh prepare.sh
 #
 # Post Installation Tasks
 #
+echo -e "\e[38;5;82m===>\e[0m Performing cleanup"
+rm -rf docker-compose-run.sh docker-compose.yml Dockerfile populate.py README.md
+echo -e "\e[38;5;82m===>\e[0m Edit nginx config"
---
+++ scripts/startup.sh
@@ -0,0 +1,1 @@
+uwsgi -s /tmp/uwsgi.sock -w "CTFd:create_app()" --chmod-socket=666
