3b907f6fd7b0499b580b8238052087bd6144ea01
semprix <semprixd@gmail.com>
2016-05-07 08:16:18 +0800

    path

--- igniter.sh
+++ igniter.sh
@@ -61,7 +61,7 @@ echo -e "\e[38;5;82m===>\e[0m Building essentials"
 apt-get install build-essential python-dev python-pip libffi-dev -y
 sleep 2
 echo -e "\e[38;5;82m===>\e[0m Fetching required python libraries"
-pip install -r requirements.txt
+pip install -r ~/CTFIgniter/requirements.txt
 echo -e "\e[38;5;82m===>\e[0m Fetching uwsgi"
 sleep 2
 pip install uwsgi
