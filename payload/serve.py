13b231ee4b6a0628e138fc7f8f2acb8590b9bf1f
semprix <semprixd@gmail.com>
2016-05-07 15:51:53 +0800

    added disclaimer

--- payload/serve.py
+++ payload/serve.py
@@ -1,3 +1,5 @@
+# do not modify this file since we are deploying it to Nginx, sockets will be used.
+
 from CTFd import create_app
 app = create_app()
 app.run(debug=True, host="0.0.0.0", port=4000)
