a255de451381b8583cf1886089b683c9758e2a97
semprix <semprixd@gmail.com>
2016-05-07 15:03:54 +0800

    minor updates

--- igniter.sh
+++ igniter.sh
@@ -6,6 +6,8 @@
 #
 #
 
+currentdir=`pwd`
+
 echo -e "\e[38;5;82m*******************************************************************************\e[0m"
 echo -e "\e[38;5;82mCTF Igniter is a fast CTF deployment tool, it will deploy Nginx and CTFd Engine\e[0m"
 echo -e "\e[38;5;82m*******************************************************************************\e[0m"
@@ -34,7 +36,7 @@ sleep 2
 echo -e "\e[38;5;82m===>\e[0m Installing Nginx"
 sleep 2
 apt-get -y install nginx
-# Install nginx
+# Install wget
 echo -e "\e[38;5;82m===>\e[0m Installing wget"
 sleep 2
 apt-get -y install wget
@@ -53,15 +55,17 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 # Switch to /var/www directory
 cd /var/www/
-# Extracting CTFd from semprix repo
+# Extract payload to www root
 echo -e "\e[38;5;82m===>\e[0m Extracting CTFd engine to www root"
-cp -R /root/CTFIgniter/payload/CTFd /var/www
+cp -R $currentdir/payload/CTFd /var/www
+sleep 2
+echo -e "\e[38;5;82m===>\e[0m Copying serve engine to www root"
+cp -R $currentdir/payload/server.py /var/www/CTFd
 sleep 2
 echo -e "\e[38;5;82m===>\e[0m Building essentials"
 apt-get install build-essential python-dev python-pip libffi-dev -y
 sleep 2
 echo -e "\e[38;5;82m===>\e[0m Fetching required python libraries"
-currentdir=`pwd`
 pip install -r $currentdir/requirements.txt
 echo -e "\e[38;5;82m===>\e[0m Fetching uwsgi"
 sleep 2
@@ -70,15 +74,24 @@ pip install uwsgi
 #
 # Post Installation Tasks
 #
-echo -e "\e[38;5;82m===>\e[0m Performing cleanup"
-rm -rf docker-compose-run.sh docker-compose.yml Dockerfile populate.py README.md
+echo -e "\e[38;5;82m[++]\e[0m Performing Post-installation"
+
 echo -e "\e[38;5;82m===>\e[0m Forcing Stop Nginx"
 killall nginx
 sleep 3
+echo -e "\e[38;5;82m===>\e[0m Copying Nginx configs"
+cp $currentdir/conf/ctfd.nginx.conf /etc/nginx/sites-available/ctfd
+echo -e "\e[38;5;82m===>\e[0m creating symbolic links"
+ln -s /etc/nginx/sites-available/ctfd /etc/nginx/sites-enabled/ctfd
+sleep 3
+echo -e "\e[38;5;82m===>\e[0m Performing cleanup"
+rm -rf /etc/nginx/sites-available/default
+rm -rf /etc/nginx/sites-enabled/default
+rm -rf get-pip.py
 echo -e "\e[38;5;82m===>\e[0m Starting Nginx"
 service nginx start
-echo -e "\e[38;5;82m===>\e[0m Edit nginx config (use CTFIgniter/conf/ctfd.nginx.conf configuration file for Nginx)"
-echo -e "\e[38;5;82m===>\e[0m Start CTFd by using CTFIgniter/scripts/startup.sh"
+sleep 3
+echo -e "\e[38;5;82m===>\e[0m Check CTFIgniter/scripts/startup.sh how to start CTFIgniter"
 sleep 2
 echo -e "\e[38;5;82m===>\e[0m Finishing....."
 echo -e "\e[38;5;82m===>\e[0m Done"
