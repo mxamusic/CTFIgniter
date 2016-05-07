e18fcdd97fdf1a86a3b2b2b3efbbc803a5b83a5c
semprix <semprixd@gmail.com>
2016-05-08 05:31:30 +0800

    added unzip package

--- igniter.sh
+++ igniter.sh
@@ -43,6 +43,11 @@ echo -e "\e[38;5;82m===>\e[0m Installing wget"
 sleep 2
 apt-get -y install wget
 sleep 2
+# Install unzip
+echo -e "\e[38;5;82m===>\e[0m Installing unzip"
+sleep 2
+apt-get -y install unzip
+sleep 2
 # Install sudo
 echo -e "\e[38;5;82m===>\e[0m Installing sudo"
 sleep 2
